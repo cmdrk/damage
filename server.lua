7b4e07924279916b654b4c4f4ef26ca8bd0e070d
cmdrk <cmdrk@proton.me>
2025-06-01 14:08:17 -0500

    Make it a lib

--- server.lua
+++ server.lua
@@ -1,15 +1,29 @@
 Damage = require("damage")
 
+local server = {}
+
 -- Internal libs
 local mapgen = require("src.util.mapgen")
 
 -- Setup ECS filters
 local update_filter = Tiny.requireAll('is_server_system')
 
-function load_level(mapstr) 
+function server.load_level(mapstr) 
     mapgen.run(mapstr)
 end
 
-function update(dt) 
+function server.update(dt) 
     ecs:update(dt, update_filter)    
 end
+
+return server
+
+---- testing
+--local Mob = require("src.entity.mob")
+--
+--local m = Mob(2,2)
+--ecs:addEntity(m)
+--collision:add(m, m.position.x, m.position.y, m.collision_shape)
+--print("mob pos: ("..m.position.x..","..m.position.y..")")
+--update(2)
+--print("mob pos: ("..m.position.x..","..m.position.y..")")
